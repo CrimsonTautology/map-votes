9c2aa7cb996ad07a2ee1483adb0a0e362592cbe2
The Crimson Tautology <billehs+github@gmail.com>
2013-06-24 19:57:06 -0400

    Finished session logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
   attr_accessible :avatar_url, :nickname, :profile, :provider, :uid
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.nickname = auth["info"]["nickname"]
+      user.profile = auth["info"]["urls"]["Profile"]
+      user.avatar_url = auth["info"]["image"]
+    end
+  end
 end
