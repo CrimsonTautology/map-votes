5af72a9d03f2966a11810d351c53d2e980b60daa
The Crimson Tautology <billehs+github@gmail.com>
2013-06-24 20:38:47 -0400

    working css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def full_title(page_title)
+    base = "Map Votes"
+
+    if page_title.empty?
+      base
+    else
+      "#{base} | #{page_title}"
+    end
+
+  end
 end
