bd6d90b3911d91fb0e91e06b120876daf5e5ebfc
Bill <billehs+github@gmail.com>
2013-07-05 09:01:45 -0400

    Use the "expect" syntax for rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -35,4 +35,9 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  #Use new expect syntax
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
 end
