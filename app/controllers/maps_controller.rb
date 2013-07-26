d81075f2158bc1d789a2790b3843e11f79b3da82
Bill <billehs+github@gmail.com>
2013-07-26 13:28:54 -0400

    Use a before_filter to get map

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,21 +1,20 @@
 class MapsController < ApplicationController
-  before_filter :authorize, only: [:edit, :update, :new]
+  before_filter :authorize, only: [:edit, :update, :vote]
+  before_filter :find_map, only: [:show, :edit, :update, :vote]
+
   def index
     @maps = Map.find(:all, order: 'name')
     @map_types = @maps.group_by {|m| m.map_type}.sort
   end
 
   def show
-    @map = Map.find_by_name(params[:id])
     @type = @map.map_type
   end
 
   def edit
-    @map = Map.find_by_name(params[:id])
   end
 
   def update
-    @map = Map.find_by_name(params[:id])
     if @map.update_attributes(params[:map])
       redirect_to action: 'show', id: @map
     else
@@ -28,9 +27,13 @@ class MapsController < ApplicationController
     value =  0
     value =  1 if params[:type] == "up"
     value = -1 if params[:type] == "down"
-    @map = Map.find_by_name(params[:id])
     Vote.cast_vote current_user, @map, value
     redirect_to :back, notice: "Thank you for voting!"
+  end
+
+  private
+  def find_map
+    @map = Map.find_by_name(params[:id])
   end
 
 
