84d28f461ab91021a1f99a3718a32872eef4b239
Bill <billehs+github@gmail.com>
2013-07-30 14:32:43 -0400

    Api Handler

--- app/controllers/api_key_controller.rb
+++ app/controllers/api_key_controller.rb
@@ -1,10 +1,22 @@
 class ApiKeyController < ApplicationController
   def index
+    @api_keys = ApiKey.find(:all)
   end
 
   def create
+    @api_key = ApiKey.new(params[:api_key])
+
+    if @api_key.save
+      flash[:notice] = "New Key Added"
+    else
+      flash[:alert] = "Could not add key"
+    end
+    render action: :index
   end
 
   def destroy
+    ApiKey.find(params[:id]).destroy
+    flash[:notice] = "Key Deleted!"
+    render action: :index
   end
 end
