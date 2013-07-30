2262d9ce2d04e0e0cad1e0aaadf3a1476605d648
Bill <billehs+github@gmail.com>
2013-07-30 15:18:47 -0400

    Authenticate api key in http token

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,2 +1,11 @@
 class ApiController < ApplicationController
+  before_filter :check_api_key
+  respond_to :json
+
+  private
+  def check_api_key
+    authenticate_or_request_with_http_token do |token, options|
+      ApiKey.exists?(access_token: token)
+    end
+  end
 end
