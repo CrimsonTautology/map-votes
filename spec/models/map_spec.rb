cf4498600cb3dc65ec88afdee3297014f7045f8b
??? <billehs+github@gmail.com>
2013-10-17 15:53:48 -0400

    Test #find_related_maps_deep

--- spec/models/map_spec.rb
+++ spec/models/map_spec.rb
@@ -28,5 +28,28 @@ describe Map do
 
   end
 
+  describe "#find_related_maps_deep" do
+    let(:map) {FactoryGirl.create(:map, name: "cp_base_name_v1")}
+    subject{ map.find_related_maps_deep.map(&:name) }
+
+    before do
+      FactoryGirl.create(:map, name: "koth_base_name_v1")
+      FactoryGirl.create(:map, name: "cp_base_name")
+      FactoryGirl.create(:map, name: "cp_base_name_final")
+      FactoryGirl.create(:map, name: "cp_dustbowl")
+      FactoryGirl.create(:map, name: "koth_place_v1")
+      FactoryGirl.create(:map, name: "cp_secret_base_b1")
+    end
+
+    it { should include( "koth_base_name_v1") }
+    it { should include( "cp_base_name") }
+    it { should include( "cp_base_name_final") }
+
+    it { should_not include( "cp_dustbowl") }
+    it { should_not include( "koth_place_v1") }
+    it { should_not include( "cp_secret_base_b1") }
+
+  end
+
 
 end
