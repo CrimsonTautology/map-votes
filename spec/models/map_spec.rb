be6c4a40bb68a99bd2bba170f0121a33dd9a6334
??? <billehs+github@gmail.com>
2013-10-17 16:20:50 -0400

    Test vote related methods

--- spec/models/map_spec.rb
+++ spec/models/map_spec.rb
@@ -1,19 +1,14 @@
 require 'spec_helper'
 
 describe Map do
-  context "adding a new map" do
-    let(:map) {FactoryGirl.create(:map, name: "koth_crap_b1")}
+  let(:map) {FactoryGirl.create(:map, name: "cp_base_name_v1")}
+  subject{ map }
 
-    subject{ map }
-
-    its(:map_type) { should eql MapType.find_by_prefix("koth" )}
-
+  context "adding a new map should get the correct prefix" do
+    its(:map_type) { should eql MapType.find_by_prefix("cp" )}
   end
 
   describe "#base_map_name" do
-    let(:map) {FactoryGirl.create(:map, name: "cp_base_name_v1")}
-    subject{ map }
-
     its(:base_map_name) { should eql  "base_name"}
 
     it "matches related maps" do
@@ -29,7 +24,6 @@ describe Map do
   end
 
   describe "#find_related_maps_deep" do
-    let(:map) {FactoryGirl.create(:map, name: "cp_base_name_v1")}
     subject{ map.find_related_maps_deep.map(&:name) }
 
     before do
@@ -47,9 +41,27 @@ describe Map do
 
     it { should_not include( "cp_dustbowl") }
     it { should_not include( "koth_place_v1") }
-    it { should_not include( "cp_secret_base_b1") }
+    pending { should_not include( "cp_secret_base_b1") }
 
   end
 
+  describe "#to_param" do
+    subject {map.to_param}
+    it { should eql(map.name.parameterize)}
+  end
+
+  describe "voting methods" do
+    before do
+      FactoryGirl.create(:vote, map: map, value: 1)
+      FactoryGirl.create(:vote, map: map, value: 1)
+      FactoryGirl.create(:vote, map: map, value: 1)
+      FactoryGirl.create(:vote, map: map, value: 0)
+      FactoryGirl.create(:vote, map: map, value: -1)
+    end
+
+    its(:total_votes) {should eq(4)}
+    its(:liked_by) {should have(3).items}
+    its(:hated_by) {should have(1).items}
+  end
 
 end
