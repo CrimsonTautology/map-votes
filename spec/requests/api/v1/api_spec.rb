8a3b6d3f6b26d242e230101ed66b503a0103e074
??? <billehs+github@gmail.com>
2013-10-18 14:55:26 -0400

    Stubs to test the api

---
+++ spec/requests/api/v1/api_spec.rb
@@ -0,0 +1,17 @@
+describe "MapVotes API" do
+  describe "#cast_vote" do
+
+  end
+
+  describe "#write_message" do
+    before
+      post '/v1/api/write_message'
+    end
+
+  end
+
+  describe "#server_query" do
+
+  end
+
+end
