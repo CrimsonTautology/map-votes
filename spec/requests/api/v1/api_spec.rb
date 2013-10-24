9cac1be833f9251667e56f4baed6a403e00a8b37
??? <billehs+github@gmail.com>
2013-10-24 15:43:58 -0400

    Add tests for future api calls

--- spec/requests/api/v1/api_spec.rb
+++ spec/requests/api/v1/api_spec.rb
@@ -1,74 +1,82 @@
 require "spec_helper"
 
+shared_examples_for "ApiController" do |route,params|
+  context "without access_token" do
+    before {post route, params}
+    its(:code) { should eql("403")}
+  end
+  context "with invalid access_token" do
+    before do
+      ApiKey.delete_all
+      post route, params.merge({access_token: "badtoken"})
+    end
+    its(:code) { should eql("403")}
+  end
+
+  context "with valid access_token" do
+    let!(:api_key) {FactoryGirl.create(:api_key)}
+    params.each_key do |key|
+      it "should complain about missing #{key}" do
+        post route, params.except(key).merge({access_token: api_key.access_token})
+        expect(response).to be_bad_request
+      end
+    end
+
+    it "should pass" do
+      post route, params.merge({access_token: api_key.access_token})
+      expect(response).to be_success
+    end
+  end
+end
+
+share_examples_for "map query" do |route, params|
+  let!(:api_key) {FactoryGirl.create(:api_key)}
+  it "adds new maps if they do not exist in the database" do
+    expect do
+      post route, params.merge({access_token: api_key.access_token})
+    end.to change{Map.count}.by(1)
+  end
+end
+
 describe "POST /v1/api" do
   subject { response }
 
   describe "/cast_vote" do
     let!(:user) {FactoryGirl.create(:user, provider: "steam", uid: "123456")}
     let!(:map) {FactoryGirl.create(:map)}
+    route = "/v1/api/cast_vote"
 
-    context "without access_token" do
-      before {post '/v1/api/cast_vote'}
-      its(:code) { should eql("403")}
-    end
-    context "with invalid access_token" do
-      before do
-        ApiKey.delete_all
-        post '/v1/api/cast_vote',
-          access_token: "badtoken",
-          uid: user.uid,
-          map: map.name,
-          value: 1
-      end
-      its(:code) { should eql("403")}
-    end
+    it_should_behave_like "ApiController", route, {
+      uid: "123456",
+      map: "auto_map",
+      value: 1
+    }
+    it_should_behave_like "map query", route, {
+      uid: "123456",
+      map: "auto_map",
+      value: 1
+    }
 
     context "with valid access_token" do
       let!(:api_key) {FactoryGirl.create(:api_key)}
 
-      it "won't submit without a map" do
+      it "casts a vote with valid information" do
         expect do
-          post '/v1/api/cast_vote',
-            access_token: api_key.access_token,
-            uid: user.uid,
-            value: 1
-        end.to_not change{Vote.count}.by(1)
-      end
-      it "won't submit without a user id" do
-        expect do
-          post '/v1/api/cast_vote',
-            access_token: api_key.access_token,
-            map: map.name,
-            value: 1
-        end.to_not change{Vote.count}.by(1)
-      end
-      it "won't submit without a value" do
-        expect do
-          post '/v1/api/cast_vote',
-            access_token: api_key.access_token,
-            uid: user.uid,
-            map: map.name
-        end.to_not change{Vote.count}.by(1)
-      end
-
-      it "Submits with needed information" do
-        expect do
-          post '/v1/api/cast_vote',
+          post route,
             access_token: api_key.access_token,
             uid: user.uid,
             map: map.name,
             value: 1
         end.to change{Vote.count}.by(1)
       end
-
-      it "Adds new maps if they do not exist in the database" do
+      it "does not cast a vote with invalid value" do
         expect do
-          post '/v1/api/cast_vote',
+          post route,
             access_token: api_key.access_token,
             uid: user.uid,
-            map: "New_Map",
-            value: 1
-        end.to change{Map.count}.by(1)
+            map: map.name,
+            value: 292
+        end.to raise_error(ActiveRecord::RecordInvalid)
       end
 
     end
@@ -78,54 +86,25 @@ describe "POST /v1/api" do
   describe "/write_message" do
     let!(:user) {FactoryGirl.create(:user, provider: "steam", uid: "123456")}
     let!(:map) {FactoryGirl.create(:map)}
+    route = "/v1/api/write_message"
 
-    context "without access_token" do
-      before {post '/v1/api/write_message'}
-      its(:code) { should eql("403")}
-    end
-    context "with invalid access_token" do
-      before do
-        ApiKey.delete_all
-        post '/v1/api/write_message',
-          access_token: "badtoken",
-          uid: user.uid,
-          map: map.name,
-          comment: "Comment"
-      end
-      its(:code) { should eql("403")}
-    end
+    it_should_behave_like "ApiController", route, {
+      uid: "123456",
+      map: "auto_map",
+      comment: "Comment"
+    }
+    it_should_behave_like "map query", route, {
+      uid: "123456",
+      map: "New_Map",
+      comment: "Comment"
+    }
 
     context "with valid access_token" do
       let!(:api_key) {FactoryGirl.create(:api_key)}
 
-      it "won't submit without a map" do
+      it "writes a comment" do
         expect do
-          post '/v1/api/write_message',
-            access_token: api_key.access_token,
-            uid: user.uid,
-            comment: "Comment"
-        end.to_not change{MapComment.count}.by(1)
-      end
-      it "won't submit without a user id" do
-        expect do
-          post '/v1/api/write_message',
-            access_token: api_key.access_token,
-            map: map.name,
-            comment: "Comment"
-        end.to_not change{MapComment.count}.by(1)
-      end
-      it "won't submit without a comment" do
-        expect do
-          post '/v1/api/write_message',
-            access_token: api_key.access_token,
-            uid: user.uid,
-            map: map.name
-        end.to_not change{MapComment.count}.by(1)
-      end
-
-      it "Submits with needed information" do
-        expect do
-          post '/v1/api/write_message',
+          post route,
             access_token: api_key.access_token,
             uid: user.uid,
             map: map.name,
@@ -133,9 +112,9 @@ describe "POST /v1/api" do
         end.to change{MapComment.count}.by(1)
       end
 
-      it "Adds new maps if they do not exist in the database" do
+      it "adds new maps if they do not exist in the database" do
         expect do
-          post '/v1/api/write_message',
+          post route,
             access_token: api_key.access_token,
             uid: user.uid,
             map: "New_Map",
@@ -144,7 +123,7 @@ describe "POST /v1/api" do
       end
 
       it "Can read and convert a base64 url safe comment" do
-        post '/v1/api/write_message',
+        post route,
           access_token: api_key.access_token,
           uid: user.uid,
           map: map.name,
@@ -155,6 +134,70 @@ describe "POST /v1/api" do
 
     end
 
+  end
+
+  describe "/favorite" do
+    route = "/v1/api/favorite"
+    it_should_behave_like "ApiController", route, {
+      uid: "123456",
+      map: "auto_map"
+    }
+    it_should_behave_like "map query", route, {
+      uid: "123456",
+      map: "New_Map"
+    }
+
+  end
+  describe "/unfavorite" do
+    route = "/v1/api/unfavorite"
+    it_should_behave_like "ApiController", route, {
+      uid: "123456",
+      map: "auto_map"
+    }
+    it_should_behave_like "map query", route, {
+      uid: "123456",
+      map: "New_Map"
+    }
+
+  end
+  describe "/have_not_voted" do
+    route = "/v1/api/have_not_voted"
+    let!(:user1) {FactoryGirl.create(:user, provider: "steam", uid: "123456")}
+    let!(:user2) {FactoryGirl.create(:user, provider: "steam", uid: "223456")}
+    let!(:user3) {FactoryGirl.create(:user, provider: "steam", uid: "323456")}
+    let!(:map) {FactoryGirl.create(:map)}
+    let!(:other_map) {FactoryGirl.create(:map, name: "cp_other_map_b1")}
+
+    before do
+      FactoryGirl.create(:vote, user: user2, map: map, value:1)
+      FactoryGirl.create(:vote, user: user2, map: other_map, value:1)
+      FactoryGirl.create(:vote, user: user3, map: other_map, value:-1)
+    end
+
+    it_should_behave_like "ApiController", route, {
+      uids: ["123456","223456","323456"],
+      map: "auto_map",
+      players: [7,2,22]
+    }
+    it_should_behave_like "map query", route, {
+      uids: ["123456","223456","323456"],
+      map: "New_Map",
+      players: [7,2,22]
+    }
+
+    context "with valid access_token" do
+      let!(:api_key) {FactoryGirl.create(:api_key)}
+
+      it "returns players who have not voted on map" do
+        expect do
+          post route,
+            access_token: api_key.access_token,
+            uids: user.uid,
+            map: map.name,
+            comment: "Comment"
+        end.to change{MapComment.count}.by(1)
+      end
+    end
   end
 
   describe "/server_query" do
