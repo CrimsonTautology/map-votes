4bc053e07e06f9d26b9a55c7fa3a9e70443e4945
??? <billehs+github@gmail.com>
2013-11-13 16:10:53 -0500

    Display a list of new events on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    votes = Vote.order('created_at DESC').first(20)
+    comments = MapComment.order('created_at DESC').first(20)
+    maps = Map.order('created_at DESC').first(20)
+    all = (votes + comments + maps).sort{|x,y| x.created_at <=> y.created_at}.reverse.take(30)
+    @events = Event.build_list all
   end
 end
--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -34,5 +34,29 @@ describe "Static pages" do
       it { should have_content(user.avatar_url)}
       it { should have_link("Log Out", href: logout_path)}
     end
+
+    describe "Recent events" do
+      let!(:m1) {FactoryGirl.create(:map, name: "cp_map1")}
+      let!(:m2) {FactoryGirl.create(:map, name: "cp_map2")}
+      let!(:u1) {FactoryGirl.create(:user, nickname: "User1")}
+      let!(:u2) {FactoryGirl.create(:user, nickname: "User2")}
+
+      before do
+        MapComment.write_message u1, m1, "test"
+        Vote.cast_vote u1, m1, 1
+        Vote.cast_vote u1, m2, -1
+        MapComment.write_message u1, m2, "test"
+        MapComment.write_message u2, m1, "test"
+        MapComment.write_message u2, m2, "test"
+        Vote.cast_vote u2, m2, 1
+      end
+
+      it { should have_content("2 users voted on cp_map1") }
+      it { should have_content("Added new map:  cp_map1") }
+      it { should have_content("2 users commented on cp_map2") }
+      it { should have_content("User1 commented on cp_map1") }
+      it { should have_content("User2 voted on cp_map2") }
+
+    end
   end
 end
