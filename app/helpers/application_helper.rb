ea994c1264c6db2fc6c9f01fad395ae48668699c
??? <billehs+github@gmail.com>
2013-11-19 11:50:20 -0500

    Use default image url if it exists

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,34 +11,31 @@ module ApplicationHelper
 
   end
 
-  def smart_map_image_url name
-    url = "http://image.www.gametracker.com/images/maps/160x120/tf2/#{name}.jpg"
-    if url_responsive? url
-      url
-    else
-      "unkown_image.jpg"
-    end
-  end
-
   def url_responsive? url
-    uri = URI(url)
-    uri_request = Net::HTTP.new uri.host
-    uri_response= uri_request.request_head uri.path
-    uri_response.code.to_i == 200
+    begin
+      uri = URI(url)
+      uri_request = Net::HTTP.new uri.host
+      uri_response= uri_request.request_head uri.path
+      uri_response.code.to_i == 200
+    rescue URI::BadURIError
+      false
+    rescue URI::InvalidURIError
+      false
+    end
   end
 
   def bootstrap_class_for flash_type
     case flash_type
-      when :success
-        "alert-success"
-      when :error
-        "alert-error"
-      when :alert
-        "alert-block"
-      when :notice
-        "alert-info"
-      else
-        flash_type.to_s
+    when :success
+      "alert-success"
+    when :error
+      "alert-error"
+    when :alert
+      "alert-block"
+    when :notice
+      "alert-info"
+    else
+      flash_type.to_s
     end
   end
 
--- app/helpers/maps_helper.rb
+++ app/helpers/maps_helper.rb
@@ -10,4 +10,19 @@ module MapsHelper
   def ziped_file_extension map
     map.name + ".bsp.bz2"
   end
+
+  #First try provided image; than a third party source
+  def smart_map_image_url map
+    if url_responsive? map.image
+      map.image
+    else
+      url = "http://image.www.gametracker.com/images/maps/160x120/tf2/#{map.name}.jpg"
+      if url_responsive? url
+        url
+      else
+        "unkown_image.jpg"
+      end
+    end
+  end
+
 end
