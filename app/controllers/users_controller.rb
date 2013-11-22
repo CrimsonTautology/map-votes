22fee42de8c1e0477cede0bd2e9929b4f291f284
??? <billehs+github@gmail.com>
2013-11-22 14:26:43 -0500

    Update user editing logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,41 @@
 class UsersController < ApplicationController
   authorize_resource
-  before_filter :find_user, only: [:show, :ban]
+  before_filter :find_user, only: [:show, :ban, :edit, :unban, :update]
 
   def show
     authorize! :read, @user
 
   end
 
+  def edit
+  end
+  def update
+    @user.admin = params[:user][:admin]
+    @user.moderator = params[:user][:moderator]
+    if @user.save
+      redirect_to(@user)
+    else
+      flash[:alert] = "Could not update user"
+      render action: 'edit'
+    end
+
+  end
   def ban
-    #@user.banned_at = Time.now
-    #@user.save!
+    if !@user.banned?
+      @user.banned_at = Time.now
+      @user.save!
+      flash[:notice] = "Banned User"
+    else
+      flash[:alert] = "User already banned"
+
+    end
+    redirect_to(@user)
   end
   def unban
+    @user.banned_at = nil
+    @user.save!
+    flash[:notice] = "Unbanned User"
+    redirect_to(@user)
   end
 
   private
